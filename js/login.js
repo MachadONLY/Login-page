444984b26721c991405a060b4f3df1fd3831f50c
tlucass <rm97913@fiap.com.br>
2023-06-01 19:02:05 -0300

    adicionando login

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginButton = document.getElementById('login-button');
+
+if (localStorage.getItem('isLoggedIn') === 'true') {
+  //localStorage.removeItem('isLoggedIn');
+  window.location.href = 'https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021';
+}
+
+loginButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  if (username === 'Admin' && password === '123456') {
+    // Verificar se a opção "Lembrar de mim" está marcada
+    const rememberMe = document.getElementById('rememberMe').checked;
+
+    // Definir o valor correspondente no localStorage
+    if (rememberMe) {
+      localStorage.setItem('isLoggedIn', 'true');
+    } else {
+      localStorage.removeItem('isLoggedIn');
+    }
+
+    // Redirecionar para a URL desejada
+    window.location.href = 'https://www.unicef.org/brazil/comunicados-de-imprensa/relatorio-da-onu-numeros-globais-de-fome-subiram-para-cerca-de-828-milhoes-em-2021';
+  } else {
+    alert('Usuário ou senha inválidos.');
+  }
+});
